27233c3392a79b6db1ab04f0d9d1f169b8ba38d1
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-09 04:54:09 -0600

    + index view

--- website/home/views.py
+++ website/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.conf import settings
 
 # Create your views here.
+def index(request):
+    return render_to_response('index.html', {})
