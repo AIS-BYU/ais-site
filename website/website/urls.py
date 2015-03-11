b61167d6059716b7493f271daa55a6104ea97c87
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-11 11:11:33 -0600

    Added about and sponsors URLs.

--- website/website/urls.py
+++ website/website/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'home.views.index'),
+    url(r'^about/', 'home.views.about'),
+    url(r'^sponsors/', 'home.views.sponsors'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
