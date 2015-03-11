1e3006f743fb27d29fd0c2329998ada40b1799f1
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-11 11:32:58 -0600

    Added routing for About and Sponsor pages.

--- website/home/views.py
+++ website/home/views.py
@@ -5,3 +5,9 @@ from django.conf import settings
 # Create your views here.
 def index(request):
     return render_to_response('index.html', {})
+
+def about(request):
+    return render_to_response('about.html', {})
+
+def sponsors(request):
+    return render_to_response('sponsor.html', {})
