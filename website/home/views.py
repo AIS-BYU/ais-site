fe1155db4c856bce927a7a3e04d0a4850ef446c2
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-11 22:12:31 -0600

    + Sponsor data into template

--- website/home/views.py
+++ website/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.conf import settings
+from home.models import *
 
 # Create your views here.
 def index(request):
@@ -10,4 +11,6 @@ def about(request):
     return render_to_response('about.html', {})
 
 def sponsors(request):
-    return render_to_response('sponsor.html', {})
+    sponsors = Sponsor.objects.all()
+
+    return render_to_response('sponsor.html', {'sponsors': sponsors})
