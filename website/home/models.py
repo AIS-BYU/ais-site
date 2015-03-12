607d034f5082b75d9fc19c3c0d185414fc94e922
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-11 22:12:10 -0600

    + Sponsor model

--- website/home/models.py
+++ website/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Sponsor(models.Model):
+    company_name = models.TextField(max_length=150, blank=False, null=False)
+    website = models.URLField()
+    logo_image = models.FileField(upload_to='sponsor/')
+
+    def __str__(self):
+        return self.company_name
