d73fd21c0b62abefd9fc2a71e891271d280c0052
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-13 08:02:25 -0600

    + Google+ event feed to main page

--- website/home/views.py
+++ website/home/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.conf import settings
 from home.models import *
+import requests
+import json
 
 # Create your views here.
 def index(request):
-    return render_to_response('index.html', {})
+    r = requests.get('https://www.googleapis.com/plus/v1/people/111490187039594096525/activities/public?key=AIzaSyCsZyECtnoQw2xKqlBWRs13v0_DUrC5Q5I&maxResults=3')
+
+    print(json.dumps(r.json(), indent=4, sort_keys=True))
+
+    return render_to_response('index.html', {'events': r.json()})
 
 def about(request):
     return render_to_response('about.html', {})
 
 def sponsors(request):
-    sponsors = Sponsor.objects.all()
+    sponsors = Sponsor.objects.all().order_by('name')
 
     return render_to_response('sponsor.html', {'sponsors': sponsors})
