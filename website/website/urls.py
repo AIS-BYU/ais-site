ac721b4a3822782e87d93a7b339c6571bdc23983
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-21 11:35:47 -0600

    + Events page

--- website/website/urls.py
+++ website/website/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,6 +13,8 @@ urlpatterns = patterns('',
     url(r'^about/', 'home.views.about'),
     url(r'^sponsors/', 'home.views.sponsors'),
     url(r'^calendar/', 'home.views.calendar'),
+    url(r'^events/', 'home.views.events'),
+    url(r'^favicon.ico$', lambda x: HttpResponseRedirect(settings.MEDIA_URL+'/favicon.ico')), #google chrome favicon fix
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
