569a5552bbd593817cc755a5cdf2e616a1242f9f
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-21 11:37:12 -0600

    + Event view definition

--- website/home/views.py
+++ website/home/views.py
@@ -4,10 +4,11 @@ from django.conf import settings
 from home.models import *
 import requests
 import json
+from website.settings import GOOGLE_PLUS_KEY
 
 # Create your views here.
 def index(request):
-    r = requests.get('https://www.googleapis.com/plus/v1/people/111490187039594096525/activities/public?key=AIzaSyCsZyECtnoQw2xKqlBWRs13v0_DUrC5Q5I&maxResults=3')
+    r = requests.get('https://www.googleapis.com/plus/v1/people/111490187039594096525/activities/public?key={0}&maxResults=3'.format( GOOGLE_PLUS_KEY ))
 
     return render_to_response('index.html', {'events': r.json()})
 
@@ -21,3 +22,9 @@ def sponsors(request):
 
 def calendar(request):
     return render_to_response('calendar.html')
+
+def events(request):
+    r = requests.get('https://www.googleapis.com/plus/v1/people/111490187039594096525/activities/public?key={0}&maxResults=10'.format( GOOGLE_PLUS_KEY ))
+
+    print(r.json())
+    return render_to_response('events.html', {'events': r.json()})
