a632a81be54639b0e29f803cfca3a0a481547338
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-03-23 08:39:09 -0600

    Divided sponsors into sponsorship levels (tiers)

--- website/home/views.py
+++ website/home/views.py
@@ -10,21 +10,30 @@ from website.settings import GOOGLE_PLUS_KEY
 def index(request):
     r = requests.get('https://www.googleapis.com/plus/v1/people/111490187039594096525/activities/public?key={0}&maxResults=3'.format( GOOGLE_PLUS_KEY ))
 
-    return render_to_response('index.html', {'events': r.json()})
+    return render_to_response('index.html', {'events': r.json(), 'request': request})
 
 def about(request):
-    return render_to_response('about.html', {})
+    return render_to_response('about.html', {'request': request})
 
 def sponsors(request):
     sponsors = Sponsor.objects.all().order_by('name')
 
-    return render_to_response('sponsor.html', {'sponsors': sponsors})
+    platinum_sponsors = sponsors.filter(sponsorship_level='platinum')
+    gold_sponsors = sponsors.filter(sponsorship_level='gold')
+    silver_sponsors = sponsors.filter(sponsorship_level='silver')
+
+    return render_to_response('sponsor.html', {
+        'sponsors': sponsors,
+        'request': request,
+        'platinum_sponsors': platinum_sponsors,
+        'gold_sponsors': gold_sponsors,
+        'silver_sponsors': silver_sponsors,
+        })
 
 def calendar(request):
-    return render_to_response('calendar.html')
+    return render_to_response('calendar.html', {'request': request})
 
 def events(request):
     r = requests.get('https://www.googleapis.com/plus/v1/people/111490187039594096525/activities/public?key={0}&maxResults=10'.format( GOOGLE_PLUS_KEY ))
 
-    print(r.json())
-    return render_to_response('events.html', {'events': r.json()})
+    return render_to_response('events.html', {'events': r.json(), 'request': request})
