eb2fea84a84538cbf5bc24c5e59983fb86e8a6cc
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-04-04 11:00:28 -0600

    + AisOfficer model

--- website/home/models.py
+++ website/home/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Sponsor(models.Model):
-    name              = models.TextField(max_length=150, blank=False, null=False)
-    website           = models.URLField()
-    description       = models.TextField(max_length=500, blank=True, null=True)
-    logo_image        = models.FileField(upload_to='sponsor/')
-    sponsorship_level = models.TextField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
 class SiteUser(AbstractUser):
     fullname   = models.TextField(blank=True, null=True)
     phone      = models.TextField(blank=True, null=True)
@@ -24,13 +14,27 @@ class SiteUser(AbstractUser):
             return '%s %s' % (self.first_name, self.last_name)
         return self.fullname
 
+class AisOfficer(models.Model):
+    user  = models.ForeignKey(SiteUser)
+    title = models.TextField()
+    email = models.EmailField()
+    lnkdn = models.URLField()
+    twttr = models.URLField()
+
+class Sponsor(models.Model):
+    name              = models.TextField(max_length=150, blank=False, null=False)
+    website           = models.URLField()
+    description       = models.TextField(max_length=500, blank=True, null=True)
+    logo_image        = models.FileField(upload_to='sponsor/')
+    sponsorship_level = models.TextField(max_length=50)
+    liaison           = models.ForeignKey(AisOfficer, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class AisEvent(models.Model):
     title    = models.TextField(max_length=150)
     location = models.CharField(max_length=100, blank=True, null=True)
     sponsor  = models.ForeignKey(Sponsor)
     start    = models.DateTimeField()
     end      = models.DateTimeField()
-
-class AisOfficer(models.Model):
-    user = models.ForeignKey(SiteUser)
-    title = models.TextField()
